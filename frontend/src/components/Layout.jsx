26ac65e1c70e81e2f6399c37ad3fc6f28a3fdf78
cyrus-111 <cyrusnadar2007@gmail.com>
2026-06-19 01:25:53 +0530

    made some updates in layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,12 +3,20 @@ import { styles } from '../assets/dummyStyles';
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
-const Layout = ({ onLogout, user}) => {
-    const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+const Layout = ({ onLogout, user }) => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   return (
-    <div className= {styles.layout.root}>
-      <Navbar user = {user}  onLogout = { onLogout}/>
-      <Sidebar user ={user} isCollapsed = {sidebarCollapsed} setIsCollapsed = {setSidebarCollapsed}/>
+    <div className={styles.layout.root}>
+      <Navbar user={user} onLogout={onLogout} />
+      <Sidebar user={user} isCollapsed={sidebarCollapsed} setIsCollapsed={setSidebarCollapsed} />
+      <div className={styles.layout.mainContainer(sidebarCollapsed)}>
+        <div className={styles.header.container}>
+          <div>
+            <h1 className={styles.header.title}>Dashboard</h1>
+            <p className={styles.header.subtitle}>Welcome Back!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
